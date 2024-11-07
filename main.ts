e69efec0503997e388823eaeaaf1462777a3a4d0
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-11-07 11:00:49 -0500

    placed beta radio code and edited flowchart

--- main.ts
+++ main.ts
@@ -1,8 +1,44 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Francisco Rocco Allegri
+ * Created on: SOct 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+
+let distanceToObject: number = 10
+radio.setGroup(1)
+basic.showIcon(IconNames.Happy)
+
+while (true) 
+{
+
+    distanceToObject = sonar.ping
+        (
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+    basic.showNumber(distanceToObject)
+
+    radio.onReceivedString(function (receivedString) 
+    {
+        basic.clearScreen()
+        basic.showString(receivedString)
+        basic.showIcon(IconNames.Happy)
+    })
+
+    if (distanceToObject < 10) 
+    {
+        basic.showIcon(IconNames.Triangle)
+        radio.sendString("Too close!")
+        basic.showIcon(IconNames.Happy)   
+    }
+    
+    else 
+    {
+        
+    }
+
+}
